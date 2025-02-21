004939fb33ab70ffe118613885695eb4bae7f1d0
samantha-eva <samanthathiebaut@gmail.com>
2025-02-21 16:14:49 +0100

    connexion à a la bdd

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { connectToServer } from "./db/connection.js"; // ✅ Correct import
-import posts from "./routes/posts.js";
+import { connectToServer } from "./db/connection.js"; // ✅ Correct import;
 
 
 const PORT = process.env.SERVER_PORT || 5050;
@@ -9,7 +8,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(posts);
 
 app.get("/", (req, res) => {
   res.send("Le serveur Express est prêt!");
